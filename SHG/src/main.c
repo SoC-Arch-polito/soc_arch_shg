23024e8d7068e7ba8f3dc11a9eeb353404e7839f
Mauro Perra <s269105@studenti.polito.it>
2021-01-01 18:40:50 +0100

    tryng something

--- SHG/src/main.c
+++ SHG/src/main.c
@@ -1,67 +1,30 @@
-#include "stm32f4xx_ll_bus.h"
-#include "stm32f4xx_ll_rcc.h"
-#include "stm32f4xx_ll_system.h"
-#include "stm32f4xx_ll_utils.h"
-#include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_hal.h"
 
-#define LED_PIN                 LL_GPIO_PIN_5
-#define LED_GPIO_PORT           GPIOA
-#define LED_GPIO_CLK_ENABLE()   LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA)
-
-void Configure_GPIO(void)
-{
-  LED_GPIO_CLK_ENABLE();
-  LL_GPIO_SetPinMode(LED_GPIO_PORT, LED_PIN, LL_GPIO_MODE_OUTPUT);
-}
-
-void SystemClock_Config(void)
-{
-  /* Enable HSE oscillator */
-  LL_RCC_HSE_EnableBypass();
-  LL_RCC_HSE_Enable();
-  while(LL_RCC_HSE_IsReady() != 1)
-  {
-  };
-
-  /* Set FLASH latency */
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_3);
-
-  /* Main PLL configuration and activation */
-  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 400, LL_RCC_PLLP_DIV_4);
-  LL_RCC_PLL_Enable();
-  while(LL_RCC_PLL_IsReady() != 1)
-  {
-  };
-
-  /* Sysclk activation on the main PLL */
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
-  {
-  };
-
-  /* Set APB1 & APB2 prescaler */
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
-
-  /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
-  SystemCoreClockUpdate();
-  
-  LL_Init1msTick(SystemCoreClock);
-}
-
-
+#define LED_PIN GPIO_PIN_5
+#define LED_GPIO_PORT GPIOA
+#define LED_GPIO_CLK_ENABLE() __HAL_RCC_GPIOA_CLK_ENABLE()
 
 int main(void)
 {
-  SystemClock_Config();
-  
-  Configure_GPIO();
-  while (1)
-  {
-    LL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
-    LL_mDelay(1000);
-  }
+HAL_Init();
+LED_GPIO_CLK_ENABLE();
+GPIO_InitTypeDef GPIO_InitStruct;
+GPIO_InitStruct.Pin = LED_PIN;
+GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+GPIO_InitStruct.Pull = GPIO_PULLUP;
+GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct); 
+
+while (1)
+{
+HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+HAL_Delay(1000);
+}
+}
+
+void SysTick_Handler(void)
+{
+HAL_IncTick();
 }
 
 void NMI_Handler(void)
@@ -70,29 +33,27 @@ void NMI_Handler(void)
 
 void HardFault_Handler(void)
 {
-  while (1) {}
+while (1) {}
 }
-
 
 void MemManage_Handler(void)
 {
-  while (1) {}
+while (1) {}
 }
 
 void BusFault_Handler(void)
 {
-  while (1) {}
+while (1) {}
 }
 
 void UsageFault_Handler(void)
 {
-  while (1) {}
+while (1) {}
 }
 
 void SVC_Handler(void)
 {
 }
-
 
 void DebugMon_Handler(void)
 {
