daf4d6deb5eb27cefe42631d1732ebeb6e1b2f1d
Luca Marongiu s269522 <s269522@studenti.polito.it>
2021-02-19 12:03:36 +0100

    Command console working configuration

--- SHG/src/modules/inc/CommandConsole.h
+++ SHG/src/modules/inc/CommandConsole.h
@@ -2,7 +2,12 @@
 #define _COMMAND_CONSOLLE_H
 #define WELCOME_STRING "******Welcome! This is the Smart Hydroponic Greenhouse!******\n\r"
 #define PRESENTATION_STRING "This is the Command Console of the SHG, take care of your plants and enjoy!.\n\r"
-#define UNKNOWNCOMMAND "Command not recognized, please write an available command, to have the list use the command 'help'.\n\r"
+#define UNKNOWNCOMMAND "Command not recognized, please write an available command.\n\r"
+#define RESETMACHINE "The machine is on reset, the default values of the thresholds are set.\n\r"
+#define TEMPSTRINGTHS "Please, insert a threshold for the temperature (from 0 to 100), and then confirm (Ok1):\n\r"
+#define HUMSTRINGTHS "Please, insert a threshold for the humidity (from 0 to 100), and then confirm (Ok2):\n\r"
+#define LIGHTSTRINGTHS "Please, insert a threshold for the light (from 0 to 100), and then confirm (Ok3):\n\r"
+#define CONFIRMSTRING "Values accepted!\n\r"
 #define COMMANDLENGTH 12
 
 /*****************COMMANDS TODO*****************/
@@ -18,12 +23,9 @@
 /***********************************************/
 // Shared Variable used to set the command.
 enum command {
-  wait_pair, pair, start, status, clear, stop, setThs, getThs, reset, error
+  wait_pair, pair, start, status, stop, setThs, getThs, reset, error, endThs = 130
 }; 
 
     void CommandConsole(void const * argument);
-    void CMD_commClear();
-    void showCommands();
-    void setTresholds(int Temperature, int Humidity, int Light);
 
 #endif
--- SHG/src/modules/src/CommandConsole.c
+++ SHG/src/modules/src/CommandConsole.c
@@ -27,7 +27,6 @@
 /*
 - start         -> to start the SHG (read from sensors and write to actuators);
 - status        -> to outupt the values of the environmental variables;
-- clear         -> clear the UART command line;
 - stop          -> pause the SHG;
 - setThs        -> force the three thresholds to a user defined thresholds;
 - getThs        -> print the three thresholds to the user;
@@ -40,11 +39,17 @@ enum command ParseCommand(uint8_t cmd);
 void CommandConsole(void const * argument)
 {
   enum command cmd;
+  uint8_t commths = 0;
+  uint8_t prev_commths = 0;
   uint8_t comm = 0;
   uint8_t prev_comm = 1;
   int repeated = 0;
   int paired = 0;
   int already_started = 0;
+  int Temperature, Humidity, Light;
+  int Temperatureths, Humidityths, Lightths;
+  char buf[10];
+  char buffer[256];
 
   /* USER CODE BEGIN CommandConsole */
   /* Infinite loop */
@@ -52,6 +57,7 @@ void CommandConsole(void const * argument)
 if(TM_I2C_IsDeviceConnected(I2C1, BLUETOOTH_RX_ADDRESS)==TM_I2C_Result_Ok){
   while(1){
        TM_I2C_ReadNoRegister(I2C1, BLUETOOTH_RX_ADDRESS, &comm);
+       //repeated is used in order to avoid multiple instances of the same command in sequence
         if(prev_comm != comm){
           repeated = 0;
         } else {
@@ -59,38 +65,121 @@ if(TM_I2C_IsDeviceConnected(I2C1, BLUETOOTH_RX_ADDRESS)==TM_I2C_Result_Ok){
         }
 
       cmd = ParseCommand(comm);
-      if(cmd == status){}
-        //bla bla
+      //handle status command
+      if(cmd == status){
+        Temperature = SHG_getTemperature();
+        Light = SHG_getLight();
+        Humidity = SHG_getHumidity();
+        strcat(buffer,"The current temperature is: ");
+        strcat(buffer,itoa(Temperature,buf,10));
+        strcat(buffer,"\n");
+        TM_USART_Puts(USART2,buffer);
+
+        strcat(buffer,"The current humidity is: ");
+        strcat(buffer,itoa(Humidity,buf,10));
+        strcat(buffer,"\n");
+        TM_USART_Puts(USART2,buffer);
+
+        strcat(buffer,"The current light is: ");
+        strcat(buffer,itoa(Light,buf,10));
+        strcat(buffer,"\n");
+        TM_USART_Puts(USART2,buffer);
+
+        memset(buffer,0,256);
+      }
+
+      //handle pair command, you can  pair the device only if you have not already paired it.
       if(cmd == pair && !paired && !repeated){
           TM_USART_Puts(USART2,"The Device is paired!\n");
           paired = 1;
       }
+
+      //handle wait_pair command
       if(cmd == wait_pair && !paired && !repeated){
           TM_USART_Puts(USART2,"The Device is pairing with the user...\n");
       }
-      if(!repeated && paired && (cmd != status) && cmd != wait_pair && cmd != pair){
-       switch (cmd){
+
+      //handle the remaining commands, you can enter only if you have paired the device
+      if(!repeated && paired && cmd != status && cmd != wait_pair && cmd != pair){
+        switch (cmd){
          case start:
-          if(!already_started)
+          if(!already_started){
+           SHG_start();
            TM_USART_Puts(USART2,WELCOME_STRING);
            TM_USART_Puts(USART2,PRESENTATION_STRING);
+          }
           already_started = 1;
            break;
-         case clear:
-           break;
          case stop:
+           already_started = 0;
+           SHG_stop();
            break;
          case setThs:
-         //l'utente deve mandare th1 e 'ok' (codice x es 110), th2 e 'ok', th3 e 'ok'
+         //l'utente deve mandare th1 e 'ok1' (110), th2 e 'ok2' (120), th3 e 'ok3' (130)
+           TM_USART_Puts(USART2,TEMPSTRINGTHS);
+           while(1){
+              TM_I2C_ReadNoRegister(I2C1, BLUETOOTH_RX_ADDRESS, &commths);
+              if(commths == 110)
+                break;
+              prev_commths = commths;
+           }
+           Temperatureths = prev_commths; 
+           SHG_setTresholdTemperature(Temperatureths);
+
+           TM_USART_Puts(USART2,HUMSTRINGTHS);
+           while(1){
+              TM_I2C_ReadNoRegister(I2C1, BLUETOOTH_RX_ADDRESS, &commths);
+              if(commths == 120)
+                break;
+              prev_commths = commths;
+           }
+           Humidityths = prev_commths; 
+           SHG_setTresholdHumidity(Humidityths);
+           
+           TM_USART_Puts(USART2,LIGHTSTRINGTHS);
+           while(1){
+              TM_I2C_ReadNoRegister(I2C1, BLUETOOTH_RX_ADDRESS, &commths);
+              if(commths == endThs)
+                break;
+              prev_commths = commths;
+           }
+           Lightths = prev_commths; 
+           SHG_setTresholdLight(Lightths);
+
+           TM_USART_Puts(USART2,CONFIRMSTRING);
            break;
          case getThs:
+          Temperature = SHG_getTresholdTemperature();
+          Light = SHG_getTresholdLight();
+          Humidity = SHG_getTresholdHumidity();
+          strcat(buffer,"The current temperature threshold is: ");
+          strcat(buffer,itoa(Temperature,buf,10));
+          strcat(buffer,"\n");
+          TM_USART_Puts(USART2,buffer);
+          memset(buffer,0,256);
+
+          strcat(buffer,"The current humidity threshold is: ");
+          strcat(buffer,itoa(Humidity,buf,10));
+          strcat(buffer,"\n");
+          TM_USART_Puts(USART2,buffer);
+          memset(buffer,0,256);
+
+          strcat(buffer,"The current light threshold is: ");
+          strcat(buffer,itoa(Light,buf,10));
+          strcat(buffer,"\n");
+          TM_USART_Puts(USART2,buffer);
+          memset(buffer,0,256);
            break;
          case reset:
+           TM_USART_Puts(USART2,RESETMACHINE);
+           SHG_rst();
            break;
+         case endThs: 
+          break;
          default:
            TM_USART_Puts(USART2,UNKNOWNCOMMAND);
            break;
-       }
+        }
       }
      osDelay(1000);
      prev_comm = comm;
@@ -112,15 +201,15 @@ enum command ParseCommand(uint8_t cmd) {
     } else if(cmd == 3){
       SHG_cmd = status;
     } else if(cmd == 4){
-      SHG_cmd = clear;
+      SHG_cmd = stop;
     } else if(cmd == 5){
-      SHG_cmd = stop;
-    } else if(cmd == 6){
       SHG_cmd = setThs;
-    } else if(cmd == 7){
+    } else if(cmd == 6){
       SHG_cmd = getThs;
-    } else if(cmd == 8){
-      SHG_cmd = stop;
+    } else if(cmd == 7){
+      SHG_cmd = reset;
+    } else if (cmd == 130){
+      SHG_cmd = endThs;
     }
     return SHG_cmd;
 }
